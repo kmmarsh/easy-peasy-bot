951cab2ad81234efe608f37da8333cee0ade2bc5
Kristen Marsh <kmarsh2@collab.net>
2018-08-09 16:31:44 -0400

    WIP
    working functionality: /timer, /break, individual todo list, profile,
    dnd, texts

--- index.js
+++ index.js
@@ -86,7 +86,6 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
     process.exit(1);
 }
 
-var todoList = ["Drink Water", "Fight a Bear", "Eat tacos", "Harass Kevin at www.slack.com"];
 
 
 /**
@@ -125,6 +124,7 @@ controller.hears(
     function(bot, message) {
         bot.startConversation(message, function(err, convo){
             convo.addQuestion("Hello! What is your name?", function(response, convo){
+                console.log(message.user);
                 controller.storage.users.get(message.user, function(err, user_data) {
                     if (!err) {
                         controller.storage.users.save(
@@ -132,12 +132,12 @@ controller.hears(
                                 ...user_data, 
                                 name: response.text
                             });
-                    }
+                    } else {console.log("Number one" + err)}
                 });
                 convo.sayFirst("Hi " + response.text);
                 convo.next();
             })
-            convo.addQuestion("We can set up text notifications now. You will only get them upon request. Say \'no\' to skip this step. What is your phone number?", 
+            convo.addQuestion("We can set up SMS text notifications now. You will only get them upon request. Say \'no\' to skip this step. What is your phone number?", 
             function(response, convo){
                 if(response.text != 'no' || response.text != "No" || response.text != "NO"){
                     controller.storage.users.get(message.user, function(err, user_data) {
@@ -147,17 +147,16 @@ controller.hears(
                                     ...user_data, 
                                     phoneNumber: response.text
                                 });
-                        }
+                        }else {console.log("Number twp: " + err)}
                     });
-                } else {
-                    setUpProfile(message.user, userName, userPhoneNumber, userCarrier);                
                 }
                 convo.next();
             })
-            convo.addQuestion("If you want to set up text notifications, could you also tell me your cell phone carrrier? Say 'no' to skip this step. Supported carriers include Alltell, AT&T, Nextel, Sprint, SunCom, T-mobile, and Verizon.",
+            convo.say("If you want to set up text notifications, could you also tell me your cell phone carrrier? Say 'no' to skip this step.");
+            convo.addQuestion("The following are supported carriers: Alltell, AT&T, Nextel, Sprint, SunCom, T-mobile, and Verizon. Please enter your carrier, excluding non-alphanumeric characters such as & or -.",
             function(response, convo){
                 if(response.text != 'no' || response.text != "No" || response.text != "NO"){
-                    userCarrier = carrierMap[response.text];
+                    userCarrier = carrierMap[response.text.toLowerCase()];
                     controller.storage.users.get(message.user, function(err, user_data) {
                         if (!err) {
                             controller.storage.users.save(
@@ -165,61 +164,45 @@ controller.hears(
                                     ...user_data, 
                                     carrier: userCarrier,
                                 });
-                        }
+                        }else {console.log("Number three:" + err)}
                     });
-                    setUpPhone(message.user, userName, userPhoneNumber, userCarrier);
+                    setUpPhone(message);
+                    convo.next();
                 }
-                convo.next();
+                convo.say("Your profile has been set up. You will recieve a text notification if your SMS setup was successful. If you do not recieve a message, try again by saying 'set up profile'."); 
+                console.log(message.user);
+                controller.storage.users.get(message.user, function(err, user_data) {
+                    if (!err) {
+                        controller.storage.users.save(
+                            {
+                                ...user_data, 
+                                toDoList: ["Complete Profile Setup"],
+                            });
+                            convo.say("Here is what your current Todo List looks like! \n" + formatToDoList(user_data.toDoList));
+                    }else {console.log("FOUR" + err)}
+                });
+                convo.say("You can say things like 'todo list' to view your list, or things like 'add' or 'remove' to edit that list.");
+                convo.say("You can also use slash commands like /timer and /break to time your work and breaks! Good luck!");
+                
             })
-            convo.say("Your profile has been set up. You will recieve a text notification if your SMS setup was successful. If you do not recieve a message, try again by saying 'set up profile'."); 
-            convo.say("You can say things like 'todo list' to view your list, or things like 'add' or 'remove' to edit that list. You can use slash commands like /timer and /break to time your work and breaks! Good luck!");
+
         })
 });
 
-function saveName(name){
-    userName = name;
-    console.log("userName; " + userName);
-}
-
-function saveNumber(number){
-    userPhoneNumber = number;
-    console.log("userPhoneNumber: " + userPhoneNumber);
-}
-
-function saveCarrier(carrier){
-    userCarrier = carrierMap[carrier];
-    console.log("userCarrier: " + userCarrier);
-}
-
-function setUpProfile(user, name, phone, carrier){
-    console.log("In setUpProfile!");
-    var profile = {
-        name: name,
-        phoneNumber: phone + carrier,
-        carrierExt: carrier,
-
-    }
-    userProfiles[user] = profile;
-    if(phone && carrier){
-        setUpPhone(userProfiles[user]);
-    }
-    userName = undefined;
-    userPhoneNumber = undefined;
-    userCarrier = undefined;
-}
-
-function setUpPhone(profile){
-    mailOptions.to = profile.phoneNumber;
-    mailOptions.subject = "Confirmation Message";
-    mailOptions.text = "Your text notifications were set up successfully!"
-    console.log("setting up that phone");
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    })
+function setUpPhone(message){
+    controller.storage.users.get(message.user, function(err, user_data) {
+        mailOptions.to = [user_data.phoneNumber, user_data.carrier].join('');
+        mailOptions.subject = "Confirmation Message";
+        mailOptions.text = "Your text notifications were set up successfully!"
+        console.log("setting up that phone");
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        })
+    });
 }
 
 
@@ -230,11 +213,29 @@ controller.hears(
     function(bot, message) {
         bot.startConversation(message, function(err, convo){
             convo.say("Hello!");
-            convo.say("Here is your To-do List: \n" + formatToDoList(todoList))
+            controller.storage.users.get(message.user, function(err, user_data) {
+                if (!err) {
+                        convo.say("Here is your Todo List: " + formatToDoList(user_data.toDoList));
+                } else {
+                    console.log(err);
+                }
+            });
             convo.addQuestion("Would you like to start today over with a blank list?", function(response,convo){
                 if(response.text == 'yes'|| response.text == 'Yes'){
-                    todoList = [];
-                    convo.sayFirst("Here is your empty Todo List: " + formatToDoList(todoList));
+                    controller.storage.users.get(message.user, function(err, user_data) {
+                        if (!err) {
+                            controller.storage.users.save(
+                                {
+                                    ...user_data, 
+                                    toDoList: [],
+                                });
+                        }
+                    });
+                    controller.storage.users.get(message.user, function(err, user_data) {
+                        if (!err) {
+                                convo.sayFirst("Here is your empty Todo List: " + formatToDoList(user_data.toDoList));
+                        }
+                    });
                     convo.next();
                 }
                 convo.next();
@@ -244,11 +245,21 @@ controller.hears(
                 if(response.text == 'yes'|| response.text == 'Yes'){
                     convo.addQuestion("Please enter the new items for your Todo List, separated by a comma.", function(response, convo){
                         convo.next();
-                        var parseItems = response.text.split(',');
-                        parseItems.forEach(element => {
-                            todoList.push(element);
+                        var parseItems = response.text.split(/, */);
+                        controller.storage.users.get(message.user, function(err, user_data) {
+                            if (!err) {
+                                controller.storage.users.save(
+                                    {
+                                        ...user_data, 
+                                        toDoList: [...user_data.toDoList, ...parseItems],
+                                    });
+                            }
                         });
-                        convo.say("Thanks! Your todo list is now \n" + formatToDoList(todoList));
+                        controller.storage.users.get(message.user, function(err, user_data) {
+                            if (!err) {
+                                convo.say("Thanks! Your todo list is now \n " + formatToDoList(user_data.toDoList.concat(parseItems)));
+                            }
+                        });
                     });
                 }
                 convo.next();
@@ -263,8 +274,11 @@ controller.hears(
     ['todo', 'to-do', 'list', 'yes', 'view', 'show'],
     ['direct_mention', 'mention', 'direct_message'],
     function(bot, message) {
-        console.log(message.user);
-        bot.reply(message, "Here is your To-do List: \n" + formatToDoList(todoList));
+        controller.storage.users.get(message.user, function(err, user_data) {
+            if (!err) {
+                bot.reply(message, "Here is your To-do List: \n" + formatToDoList(user_data.toDoList));
+            }
+        });
     }
 );
 
@@ -274,14 +288,27 @@ controller.hears(
     ['direct_mention', 'mention', 'direct_message'],
     function(bot, message) {
         bot.startConversation(message, function(err, convo){
-            convo.addQuestion("Would you like to add an item to your Todo List?", function(response, convo){
-                if(response.text == "yes" || response.text == "Yes"){
-                    convo.next();
-                    convo.addQuestion("What would you like to add to your todo list?", function(response,convo){
-                        todoList.push(response.text);
-                        convo.say("Thanks! Your todo list is now \n" + formatToDoList(todoList));
+            convo.addQuestion("Would you like to add anything to your Todo list?", function(response,convo){
+                convo.next();
+                if(response.text == 'yes'|| response.text == 'Yes'){
+                    convo.addQuestion("Please enter the new items for your Todo List, separated by a comma.", function(response, convo){
                         convo.next();
-                    }, {}, 'default');
+                        var parseItems = response.text.split(/, */);
+                        controller.storage.users.get(message.user, function(err, user_data) {
+                            if (!err) {
+                                controller.storage.users.save(
+                                    {
+                                        ...user_data, 
+                                        toDoList: [...user_data.toDoList, ...parseItems],
+                                    });
+                            }
+                        });
+                        controller.storage.users.get(message.user, function(err, user_data) {
+                            if (!err) {
+                                convo.say("Thanks! Your todo list is now \n " + formatToDoList(user_data.toDoList.concat(parseItems)));
+                            }
+                        });
+                    });
                 }
                 convo.next();
             });
@@ -298,15 +325,26 @@ controller.hears(
                 convo.addQuestion("Would you like to remove an item from the Todo List?", function(response,convo){
                     convo.next();
                     if(response.text == "yes" || response.text == "Yes"){
-                        convo.say("Okay, Here is your To-do List: \n" + formatToDoList(todoList));
-                        convo.addQuestion("Which item number would you like to remove?", function(response, convo){
-                            var index = response.text;
-                            console.log("Index: " + index);
-                            if (index > -1) {
-                                todoList.splice(index-1, 1);
+                        controller.storage.users.get(message.user, function(err, user_data) {
+                            if (!err) {
+                                    convo.sayFirst("Okay, Here is your Todo List \n" + formatToDoList(user_data.toDoList));
                             }
+                        });
+                        convo.addQuestion("Which item number would you like to remove?", function(response, convo){
                             convo.next();
-                            convo.say("Thanks! Your todo list is now \n" + formatToDoList(todoList));
+                            var index = response.text;
+                            controller.storage.users.get(message.user, function(err, user_data) {
+                                if (!err) {
+                                    var toDo = user_data.toDoList;
+                                    toDo.splice(index-1,1);
+                                    controller.storage.users.save(
+                                        {
+                                            ...user_data, 
+                                            toDoList: toDo,
+                                        });
+                                        convo.say("Thanks! Your todo list is now \n" + formatToDoList(toDo));
+                                }
+                            });
                         })
                     }
                     convo.next();
@@ -325,15 +363,22 @@ controller.hears(
                 convo.addQuestion("Would you like to clear your entire Todo List?", function(response,convo){
                     convo.next();
                     if(response.text == "yes" || response.text == "Yes"){
-                        todoList = {};
-                        convo.say("Okay, Here is your To-do List: \n" + formatToDoList(todoList));
-                    }
-                    convo.next();
-                }, {}, 'default');
-    
+                        controller.storage.users.get(message.user, function(err, user_data) {
+                        if (!err) {
+                            controller.storage.users.save(
+                                {
+                                    ...user_data, 
+                                    toDoList: [],
+                                });
+                                convo.say("Okay, Here is your Todo List \n" + formatToDoList([]));
+                        }
+                    })
+                }
             });
+            convo.next();
+        }, {}, 'default');
+    });
             
-        });
 
 
 //formats numbered todoList in text box
@@ -378,6 +423,7 @@ function formatToDoList(todoList){
 //        }
 //        bot.reply(message, 'I heard you loud and clear boss.');
 //    });
+
 
 /*
 controller.storage.users.get(message.user, function(err, user_data) {
